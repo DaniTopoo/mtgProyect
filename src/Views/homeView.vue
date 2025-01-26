cb6d31cdfb6818b74fad6096bf83af124c4321a4
DaniTopoo <danitopoodev@gmail.com>
2025-01-26 11:54:19 +0100

    buttons home animation

--- src/Views/homeView.vue
+++ src/Views/homeView.vue
@@ -3,10 +3,13 @@
     <div class="homeBox flex justify-center rounded-t-lg">
       <p class="text-6xl font-serif">Welcome to Magic the Gathering wiki</p>
     </div>
-    <div class="homeBox grid grid-cols-3 justify-items-center rounded-b-lg">
+    <div
+      class="homeBox grid grid-cols-3 justify-items-center rounded-b-lg gap-12"
+    >
       <RouterLink
-        class="flex items-center gap-2 text-4xl text-black hover:text-orange-600"
-        v-for="link in links"
+        class="flex items-center gap-3 text-4xl text-black hover:text-orange-600 slide-in"
+        v-for="(link, i) in links"
+        :style="{ '--order': i }"
         :key="link.path"
         :to="link.path"
       >
